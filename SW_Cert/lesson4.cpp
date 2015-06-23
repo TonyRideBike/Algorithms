7e3437c494ef01a9d901b0ad907159aa3043ef7a
Tony <tony.lyu.dev@gmail.com>
2015-06-23 21:48:25 +0800

    lesson 4 Word Correction done.

--- SW_Cert/lesson4.cpp
+++ SW_Cert/lesson4.cpp
@@ -82,7 +82,7 @@ mre: more me
 #include <cstdio>
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 
 #define DEBUG1 1
 #define DEBUG2 0
@@ -95,31 +95,90 @@ int main()
     freopen("lesson4.in", "r", stdin);
 #endif
     string in;
-    map<string, int> dictionary;
+    vector<string> dictionary;
     cin >> in;
     while(in != "#")
     {
-        dictionary.insert(std::map<string, int>::value_type(in, in.length()));
+        dictionary.push_back(std::vector<string>::value_type(in));
         cin >> in;
     }
 #if DEBUG2
-    for (map<string, int>::iterator i = dictionary.begin(); 
+    for (vector<string>::iterator i = dictionary.begin();
             i != dictionary.end(); ++i)
     {
-        cout << i->first << " " << i->second << endl;
+        cout << *i << endl;
     }
 #endif
     cin >> in;
-    while(in != "#")
+    while (in != "#")
     {
-        if(dictionary.find(in) != dictionary.end())
+        bool found = false;
+        vector<string> candidates;
+        for (vector<string>::iterator i = dictionary.begin();
+                i != dictionary.end(); ++i)
         {
-            cout << in << " is correct" << endl;
+            if ((*i).length() == in.length())
+            {
+                if (in.compare(*i) == 0)
+                {
+                    cout << in << " is correct" << endl;
+                    found = true;
+                    break;
+                }
+                int counter = 0;
+                for (string::size_type ix = 0; ix != (*i).size(); ++ix)
+                {
+                    if ((*i)[ix] != in[ix])
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 1)
+                {
+                    candidates.push_back((*i));
+                }
+            }
+            else if ((*i).length() == in.length() + 1 ||
+                     (*i).length() == in.length() - 1)
+            {
+                string::size_type inx = 0, ix = 0;
+                int counter = 0;
+                while (ix <= (*i).size() && inx <= in.size())
+                {
+                    if ((*i)[ix] == in[inx])
+                    {
+                        ix++;
+                        inx++;
+                    }
+                    else if (counter == 0)
+                    {
+                        (*i).length() > in.length() ? ix++ : inx++;
+                        counter++;
+                    }
+                    else
+                    {
+                        ix++;
+                        inx++;
+                        counter++;
+                    }
+                }
+                if (counter == 1)
+                {
+                    candidates.push_back(*i);
+                }
+            }
         }
-        else
+        if (!found)
         {
-            // to-do
+            cout << in << ": ";
+            for (vector<string>::iterator i = candidates.begin();
+                    i != candidates.end(); ++i)
+            {
+                cout << *i << " ";
+            }
+            cout << endl;
         }
         cin >> in;
     }
 }
+
